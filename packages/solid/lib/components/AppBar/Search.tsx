74963eb5b33bf925e3ac21ea6fa8a957a6e78f2f
alikia2x <alikia2x@outlook.com>
2025-06-29 18:32:52 +0800

    fix: handling of `class` prop of AppBarSearchBox

--- packages/solid/lib/components/AppBar/Search.tsx
+++ packages/solid/lib/components/AppBar/Search.tsx
@@ -1,24 +1,13 @@
-import { Accessor, Component, createEffect, JSX, onMount, useContext } from "solid-js";
+import { Accessor, Component, JSX } from "solid-js";
 import { AppBarConfigExpressive } from "./index";
 import { tv } from "tailwind-variants";
 import { getClass } from "@utils/getClass";
-import { createContext } from "solid-js";
-
-const AppBarRefContext = createContext();
 
 interface AppBarSearchBoxProps extends JSX.InputHTMLAttributes<HTMLInputElement> {
     scrolling?: Accessor<boolean>;
 }
 
 export const AppBarSearchBox: Component<AppBarSearchBoxProps> = ({ scrolling, ...rest }) => {
-    const ref = useContext(AppBarRefContext);
-    createEffect(() => {
-        console.log(ref);
-    });
-    onMount(() => {
-        console.log("??/");
-    });
-
     const style = tv({
         base: "bg-surface-container-high h-14 w-[max(50%,min(100%,312px))] rounded-full px-6 focus:outline-secondary\
         focus:outline-[3px] focus:outline-offset-2",
@@ -28,15 +17,15 @@ export const AppBarSearchBox: Component<AppBarSearchBoxProps> = ({ scrolling, ..
             },
         },
     });
+    const [className, restWithoutClass] = getClass(rest);
     return (
         <div class="grow flex justify-center">
-            <input class={style({ scrolling: scrolling?.() })} {...rest} />
+            <input class={style({ scrolling: scrolling?.(), className })} {...restWithoutClass} />
         </div>
     );
 };
 
 export const SearchAppBar: Component<AppBarConfigExpressive> = ({ children, scrolling, ...rest }) => {
-    let headerRef;
     const style = tv({
         base: "bg-surface sticky w-full h-16 top-0 flex justify-between items-center duration-150",
         variants: {
@@ -47,10 +36,8 @@ export const SearchAppBar: Component<AppBarConfigExpressive> = ({ children, scro
     });
     const [className, restWithoutClass] = getClass(rest);
     return (
-        <AppBarRefContext.Provider value={headerRef}>
-            <header class={style({ className, scrolling: scrolling?.() })} {...restWithoutClass} ref={headerRef}>
-                {children}
-            </header>
-        </AppBarRefContext.Provider>
+        <header class={style({ className, scrolling: scrolling?.() })} {...restWithoutClass}>
+            {children}
+        </header>
     );
 };
