e44e4f00e9cd865e700e71f299ed2616a6e7f26e
alikia2x <alikia2x@outlook.com>
2025-07-27 15:46:03 +0800

    fix: incorrect handling in TrailingElement and TrailingElementGroup

--- packages/solid/lib/components/AppBar/index.tsx
+++ packages/solid/lib/components/AppBar/index.tsx
@@ -14,22 +14,38 @@ export interface AppBarProps extends JSX.HTMLAttributes<HTMLElement> {
 
 export const LeadingElement: Component<DivProps> = (props) => {
 	const [v, rest] = splitProps(props, ["class"]);
-	const styles = tv({
+	const style = tv({
 		base: "relative ml-1 mr-2 w-12 h-12 items-center justify-center flex"
 	});
 	return (
-		<div class={styles({ class: v.class })} {...rest}>
+		<div class={style({ class: v.class })} {...rest}>
 			{props.children}
 		</div>
 	);
 };
 
-export const TrailingElementGroup: Component<{ children?: JSX.Element }> = ({ children }) => {
-	return <div class="relative ml-2 mr-1 w-auto h-12 flex">{children}</div>;
+export const TrailingElementGroup: Component<DivProps> = (props) => {
+	const [v, rest] = splitProps(props, ["class"]);
+	const style = tv({
+		base: "relative ml-2 mr-1 w-auto h-12 flex"
+	});
+	return (
+		<div class={style({ class: v.class })} {...rest}>
+			{rest.children}
+		</div>
+	);
 };
 
-export const TrailingElement: Component<{ children?: JSX.Element }> = ({ children }) => {
-	return <div class="relative rounded-full w-12 h-12">{children}</div>;
+export const TrailingElement: Component<DivProps> = (props) => {
+	const [v, rest] = splitProps(props, ["class"]);
+	const style = tv({
+		base: "relative rounded-full w-12 h-12"
+	});
+	return (
+		<div class={style({ class: v.class })} {...rest}>
+			{rest.children}
+		</div>
+	);
 };
 
 export const AppBar: Component<AppBarProps> = (props) => {
